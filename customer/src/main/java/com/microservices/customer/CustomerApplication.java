9b39b21c525d2df037c343f00b9b3a9fd662a127
marcinpokusinski7 <marcinpokusinski@gmail.com>
2023-01-07 00:42:22 +0100

    feign client

--- customer/src/main/java/com/microservices/customer/CustomerApplication.java
+++ customer/src/main/java/com/microservices/customer/CustomerApplication.java
@@ -2,9 +2,13 @@ package com.microservices.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients(
+       basePackages = "com.microservices.clients"
+)
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- customer/src/main/java/com/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/microservices/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.microservices.customer;
 
+import com.microservices.clients.fraud.FraudCheckResponse;
+import com.microservices.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +13,7 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -23,11 +26,7 @@ public class CustomerService {
         // TODO: check if fraudster
         customerRepository.saveAndFlush(customer);
 
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
